2ea06dc98b8b1011ec37aeba0e52a2130c5fd5af
James Kirkpatrick <james.a.kirkpatrick@gmail.com>
2009-12-11 20:50:57 +0000

    just writing in the command

---
+++ proJ/data/run.sh
@@ -0,0 +1,1 @@
+~/prog/votca/csgtof/netbeans/csgtof/dist/Debug/GNU-Linux-x86/csgtof --top topol.tpr --cg "P3HT.xml;PCBM.xml" --trj confout.pdb --begin 0 --first-frame 0 --nframes 1  --listcharges list_charges.xml 
