047893d452757ba779af753644fc0dfb5a9c8d59
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-12-13 13:48:48 +0100

    tests for csg_inverse

---
+++ csg/iterative_spce/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ../functions.sh
+
+test_start "running ibm"
+# running grompp to generate tpr
+dir=$PWD
+cd ibm || test_fail
+rm -Rf done step_*
+echo "csg_inverse settings.xml" | bash -i || test_fail
+echo $tmp
+cd $dir
+test_end
+
+test_start "ibm output"
+diff ibm/CG-CG.pot.new ibm/ref/CG-CG.pot.new || test_fail
+test_end
+
+test_start "running imc"
+# running grompp to generate tpr
+dir=$PWD
+cd imc || test_fail
+rm -Rf done step_*
+echo "csg_inverse settings.xml" | bash -i || test_fail
+cd $dir
+test_end
+
+test_start "imc output"
+diff imc/CG-CG.pot.new imc/ref/CG-CG.pot.new || test_fail
+test_end
+
+tests_finished
