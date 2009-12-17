33363a818e75d422f21b3f53d67ec5efb9261ef7
James Kirkpatrick <james.a.kirkpatrick@gmail.com>
2009-12-17 15:28:40 +0000

    added topol.top

--- proJ/data/run.sh
+++ proJ/data/run.sh
@@ -1,1 +1,1 @@
-~/prog/votca/csgtof/netbeans/csgtof/dist/Debug/GNU-Linux-x86/csgtof --top topol.tpr --cg "P3HT.xml;PCBM.xml" --trj confout.pdb --begin 0 --first-frame 0 --nframes 1  --listcharges list_charges.xml 
+../../../md2qm/netbeans/proJ/dist/Debug/GNU-Linux-x86/proj --top topol.tpr --cg "PCBM.xml;P3HT.xml" --trj confout.pdb --begin 0 --first-frame 0 --nframes 1  --listcharges list_charges.xml 
