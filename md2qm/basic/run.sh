6c9890855285428d24e7a88b68dc1c28862424c5
James Kirkpatrick <james.a.kirkaptrick@gmail.com>
2010-03-10 10:58:47 +0000

    added a test for the printing of pdb files

--- md2qm/basic/run.sh
+++ md2qm/basic/run.sh
@@ -16,6 +16,15 @@ test_start "checking nblist, integrals, rates"
 diff nbl_votca.res nbl_votca.ref || test_fail
 test_end
 
+test_start "running the printing of the pdb file"
+../../../md2qm/src/tools/printqmattop --top ${datadir}/topol.tpr --cg ${datadir}/norma_ch3.xml --trj ${datadir}/traj.xtc -l ${datadir}/list_charges.xml  --nframes 3 || test_fail
+test_end
+
+test_start "checking pdb files"
+diff CGtraj.pdb.ref  CGtraj.pdb || test_fail
+test_end
+
+
 tests_finished
 
 
