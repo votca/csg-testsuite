19bdf7c2434d03ab98a533a05114777759f1b4a1
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-03-15 13:58:18 +0100

    updated mapping testcase

--- csg/mapping/run.sh
+++ csg/mapping/run.sh
@@ -8,16 +8,18 @@ grompp || test_fail
 test_end
 
 test_start "mapping tpr"
-csg_map --top topol.tpr --trj conf.gro --cg water_cg.xml --out "tpr.gro" || test_fail 
+csg_map --top topol.tpr --trj conf.gro --cg water.xml --out "tpr.gro" || test_fail 
 diff tpr.gro tpr_ref.gro || test_fail
 test_end 
 
 test_start "map pdb file"
-csg_map --top topol_pdb.xml --trj conf.gro --cg "water_cg" --out "pdb.gro" || test_fail 
+csg_map --top topol_pdb.xml --trj conf.gro --cg water.xml --out "pdb.gro" || test_fail 
+test_fail
 test_end 
 
 test_start "map xml topology, 2 molecule types"
-csg_map --top topol_rename.xml --trj conf.gro --cg "water1.xml;water2.xml" --out "xml.gro" || test_fail 
+csg_map --top topol_rename.xml --trj conf.gro --cg "water1.xml;water2.xml" --out "xml.gro" || test_fail
+diff xml.gro xml_ref.gro || test_fail 
 test_end 
 
 
