312050757c7f1471f987cdd8c00cd19b8d800a29
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-03-15 18:23:20 +0100

    corrected path

--- csg/generic/run.sh
+++ csg/generic/run.sh
@@ -11,12 +11,12 @@ fi
 test_end
 
 test_start "csg_resample: resampling"
-~/src/csg/src/tools/csg_resample --in resample.in --out resampled --grid 0:0.1:3 || test_fail
+csg_resample --in resample.in --out resampled --grid 0:0.1:3 || test_fail
 diff resampled resampled.ref || test_fail
 test_end
 
 test_start "csg_resample: fitting"
-~/src/csg/src/tools/csg_resample --in resampled --out fitted --grid 0:0.1:3 --spfit 0:1:3 || test_fail
+csg_resample --in resampled --out fitted --grid 0:0.1:3 --spfit 0:1:3 || test_fail
 diff fitted fitted.ref || test_fail
 test_end
 
