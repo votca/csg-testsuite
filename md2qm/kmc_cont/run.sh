73928a17cb6c4e1a8c646b088b6c30516178e1b1
Thorsten Vehoff <vehoff@mpip-mainz.mpg.de>
2010-03-30 15:21:52 +0200

    added kmc_cont test case - results of kmc_cont currently terrible Were fine two weeks ago, searching for reason

---
+++ md2qm/kmc_cont/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ../functions.sh
+
+datadir="../data"
+
+test_start "copying QM_files"
+cp -R ${datadir}/QM_files . || test_fail
+test_end
+
+test_start "running easyj"
+../../../md2qm/src/tools/ctp_cont_kmc --crg ${datadir}/list_charges.xml --opt ${datadir}/main.xml --nframes 1 || test_fail
+test_end
+
+test_start "checking kmc_cont results"
+diff kmc_cont.res kmc_cont.ref || test_fail
+test_end
+
+tests_finished
+
+
