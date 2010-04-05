1695ccd9d234ee31e37db790afb3e41eef8e6766
James Kirkpatrick <jk499@ic.ac.uk>
2010-04-05 15:42:43 +0100

    updated mapping to allow multi frame i/o

--- md2qm/map/run.sh
+++ md2qm/map/run.sh
@@ -9,11 +9,11 @@ cp -R ${datadir}/QM_files . || test_fail
 test_end
 
 test_start "running ctp_map"
-../../../md2qm/src/tools/ctp_map --top ${datadir}/topol.tpr --cg ${datadir}/norma_ch3.xml --trj ${datadir}/traj.xtc -l ${datadir}/list_charges.xml -o ${datadir}/main.xml --nframes 1 || test_fail
+../../../md2qm/src/tools/ctp_map --top ${datadir}/topol.tpr --cg ${datadir}/norma_ch3.xml --trj ${datadir}/traj.xtc -l ${datadir}/list_charges.xml  --nframes 3 || test_fail
 test_end
 
 test_start "checking nblist"
-diff nbl_votca.res nbl_map.ref || test_fail
+diff nbl_votca_0.res nbl_map.ref || test_fail
 test_end
 
 tests_finished
