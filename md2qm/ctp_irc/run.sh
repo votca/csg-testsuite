3d0b764e946b11eef92586888505bee122c6fbcb
Thorsten Vehoff <vehoff@mpip-mainz.mpg.de>
2010-04-21 14:39:52 +0200

    test case for ctp_irc, requires stateIn.dat being equal to the state.dat of ctp_map (of course ;))

---
+++ md2qm/ctp_irc/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ../functions.sh
+
+datadir="../data"
+
+test_start "copying QM_files"
+cp -R ${datadir}/QM_files . || test_fail
+test_end
+
+test_start "running ctp_irc"
+../../../md2qm/src/tools/ctp_irc --crg ${datadir}/list_charges.xml --opt ${datadir}/main.xml || test_fail
+test_end
+
+test_start "checking kmc_cont results"
+diff kmc_cont.res kmc_cont.ref || test_fail
+test_end
+
+tests_finished
+
+
