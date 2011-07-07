40ca17092331b2eaa7f03edb39d0163fafd9e86e
Konstantin Koschke <konstantin.koschke@gmail.com>
2011-07-07 15:07:18 +0200

    changed imc solver to numpy and adjusted reference data

--- csg/generic/run.sh
+++ csg/generic/run.sh
@@ -12,11 +12,13 @@ test_end
 
 test_start "csg_resample: resampling (akima)"
 csg_resample --in resample.in --out resampled --grid 0:0.1:3 || test_fail
+sed -i '/^#/d' resampled resampled.ref
 diff resampled resampled.ref || test_fail
 test_end
 
 test_start "csg_resample: fitting"
 csg_resample --in resampled --type cubic --out fitted --grid 0:0.1:3 --fitgrid 0:1:3 || test_fail
+sed -i '/^#/d' fitted fitted.ref
 diff fitted fitted.ref || test_fail
 test_end
 
